d96557805cb0c34483fb0d5dcb1ce6a1f4a9a658
Hu Shuyuan <kukutou_mikiya@outlook.com>
2020-03-21 15:25:03 +0800

    support poll request

--- client/client.py
+++ client/client.py
@@ -1,4 +1,7 @@
 import requests
-
-response = requests.get("http://127.0.0.1:8000/module")
-print(response.text)
+input=input('please input your command:')
+input=input.split()
+if input[0]== 'list':
+        response = requests.get("http://127.0.0.1:8000/module")
+        print(response.text)
+#elif input[0]== 'list':
--- pages/serializers.py
+++ pages/serializers.py
@@ -4,5 +4,4 @@ from .models import Module
 class ModuleSerializer(serializers.ModelSerializer):
         class Meta:
                 model=Module
-                #fields='__all__'
-                fields=('module_id','year')
+                fields='__all__'
